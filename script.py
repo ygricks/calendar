9ae547458c48688c2fb818f606aaf9d9f7b3a234
Ygrick <cujbaigor90@gmail.com>
2021-09-15 22:12:29 +0300

    check holidays, draw past

--- script.py
+++ script.py
@@ -2,19 +2,29 @@ from calendar import monthrange
 from dateutil.easter import easter as get_easter
 from datetime import date, timedelta
 
-today = date.today()
+
 def get_holydays(year):
 	holydays = {
-		date(year, 1, 1), # anul nou
-		date(year, 1, 7), # craciun pe stil vechi
-		date(year, 1, 8), # sarbatoarea craciunul pe stil vechi
-		date(year, 3, 8), # ziua internationala a femeii
-		date(year, 5, 1), # siua muncii
-		date(year, 5, 9), # ziua victoriei
-		date(year, 6, 1), # ziua copiilor
-		date(year, 8, 27), # ziua independentei
-		date(year, 8, 31), # ziua nationala a limbii
-		date(year, 12, 25), # craciun pe stil nou
+		# anul nou
+		date(year, 1, 1),
+		# craciun pe stil vechi
+		date(year, 1, 7),
+		# sarbatoarea craciunul pe stil vechi
+		date(year, 1, 8),
+		# ziua internationala a femeii
+		date(year, 3, 8),
+		# siua muncii
+		date(year, 5, 1),
+		# ziua victoriei
+		date(year, 5, 9),
+		# ziua copiilor
+		date(year, 6, 1),
+		# ziua independentei
+		date(year, 8, 27),
+		# ziua nationala a limbii
+		date(year, 8, 31),
+		# craciun pe stil nou
+		date(year, 12, 25),
 	}
 
 	# prima zi de pasti
@@ -27,15 +37,21 @@ def get_holydays(year):
 	return holydays
 
 
-def month(year, month):
+def month(year, month, today):
 	months = [
 		'Ianuarie', 'Februarie', 'Martie', 
 		'Aprilie', 'Mai', 'Iunie',
 		'Iulie', 'August', 'Septembrie',
 		'Octombrie', 'Noiembrie', 'Decembrie'
 	]
+	holydays = get_holydays(year)
 	style = {
-		'h': '\x1b[2;30;90m%s\x1b[0m'
+		# weekend
+		'w': '\x1b[2;30;90m%s\x1b[0m',
+		# holidays
+		'h': '\x1b[2;30;43m%s\x1b[0m',
+		# past
+		'p': '\x1b[2;35;40m%s\x1b[0m',
 	}
 	week_name = 'Lu Ma Mi Jo Vi Si Du'
 	week_start, week_days = monthrange(year, month)
@@ -43,26 +59,30 @@ def month(year, month):
 	title = '%s %s' % (months[month], year)
 	title_len = len(week_name) // 2 + len(title) // 2
 	lines = [title.rjust(title_len), week_name]
-	line = []
-	start = week_start * -1 + 1
-	for i in range(start, week_days + 1):
-		day = str(i).rjust(2) if i > 0 else '  '
-		if i % 7 in (4, 5):
-			# line.append('\033[96m%s\033[0m' % (day))
-			line.append(style['h'] % (day))
-			# line.append('\x1b[3;33;40m%s\x1b[0m' % (day))
+	line = ['  ' for i in range(week_start)]
 
-			# line.append('\x1b[0;35;40m%s\x1b[0m' % (day))
-			# line.append('\x1b[2;38;40m%s\x1b[0m' % (day))
-			# line.append('\x1b[3;37;40m%s\x1b[0m' % (day))
-			# all colors https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
-		else:
-			line.append(day)
-		if i % 7 == 5:
+	for week_day in range(1, week_days + 1):
+		day = date(year, month, week_day)
+		day_str = str(day.day).rjust(2)
+
+		if day in holydays:
+			day_str = style['h'] % (day_str)
+		elif day.weekday() > 4:
+			day_str = style['w'] % (day_str)
+		elif day <= today:
+			day_str = style['p'] % (day_str)
+
+		line.append(day_str)
+
+		if day.weekday() == 6:
 			lines.append(' '.join(line))
 			line = []
 	return lines
 
 
-m = month(today.year, today.month)
+# console colors 
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
+
+today = date.today()
+m = month(today.year, today.month, today)
 print("\n".join(m))
